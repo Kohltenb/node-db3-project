c6a39b4a0ec21cc58eee1eb616f828d2a9d309b0
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-21 14:37:56 -0600

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -10,7 +10,7 @@ const db = require('../../data/dbn-config')
 const checkSchemeId = async (req, res, next) => {
   try {
     const existing = await db('schemes').where('scheme_id', req.params.scheme_id)
-    .first()
+      .first()
     if (!existing) {
       next({
         status: 404,
@@ -33,7 +33,15 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if (scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ) {
+    next({ status: 400, message: 'invalid scheme_name' })
+  } else {
+    next()
+  }
 }
 
 /*
@@ -46,6 +54,18 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  const { instructions, step_number } = req.body
+  if (instructions === undefined ||
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ) {
+    const error = { status: 400, message: 'invalid step' }
+    next(error)
+  } else {
+    next()
+  }
 
 }
 
